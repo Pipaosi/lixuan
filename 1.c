fb39a4b0eb8dacb9ace38806bcb46ae738fb9ac9
Pipaosi <bukanjian@163.com>
2015-12-07 15:23:55 +0800

    2

---
+++ 1.c
@@ -0,0 +1,15 @@
+/*************************************************************************
+	> File Name: 1.c
+	> Author: 
+	> Mail: 
+	> Created Time: 一 12/ 7 15:20:08 2015
+ ************************************************************************/
+
+#include<stdio.h>
+
+int main()
+{
+    printf("hello , this is first github for mac 's test/n'");
+    printf("if this is successful, github will be OK!'n'");
+
+}
